f1c2f2c228c3410f1c19ab840f185a37cb0bedd9
shk0625 <tjfgml1238@gmail.com>
2023-03-25 05:31:08 +0900

    f

--- app.js
+++ app.js
@@ -3,11 +3,7 @@ var app=express();
 
 var routes=require('./routes/route.js');
 
-app.set('view engine','ejs');
-
-app.use(express.static(__dirname + '/public'));
-
-app.get('/',routes.home);
+app.get('/',routes.data);
 
 var port = process.env.PORT || 3000;
 
--- routes/route.js
+++ routes/route.js
@@ -1,9 +1,9 @@
-exports.home=function(req,res){
-  res.render('home');
+const users = [
+  { id: 1, name: "유저1" },
+  { id: 2, name: "유저2" },
+  { id: 3, name: "유저3" }
+ ];
+
+exports.data=function(req, res){
+  res.json({ok: true, users: users});
 }
-
-
-exports.login=function(req,res){ 
-    res.render('login');
-}
-
